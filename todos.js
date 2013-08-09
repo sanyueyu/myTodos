ee2be869d390469af58d7d8e78d3f837ff02bf9e
sanyueyu <sanyueyu@gmail.com>
2013-08-09 08:54:25 +0800

    update
    i know why view know the this.model

--- todos.js
+++ todos.js
@@ -1,64 +1,65 @@
 $(function() {
 		   //Model
-	var Todo = Backbone.Model.extend({
+	var Todo = Backbone.Model.extend({//单个任务
 		defaults:function()  {
-			return {title: "empty todo ......",
-			order: Todos.nextOrder(),
-			done: false};
+			return {title: "empty todo ......",//CONTENT
+			order: Todos.nextOrder(),//ID
+			done: false};//是否已完成
 		},	
-		toggle: function() {
+		toggle: function() {//完成转换
 			this.save({done: !this.get("done")});	
 		}
 	});		 
 	
 	//Collection
-	var TodoList = Backbone.Collection.extend({
+	var TodoList = Backbone.Collection.extend({//任务列表
 		model: Todo,
 		localStorage: new Backbone.LocalStorage("todos-backbone"),
-		done: function() {
+		done: function() {//完成的任务
 			return this.where({done: true});
 		},
-		remaining: function() {
+		remaining: function() {//未完成的任务
 			return this.where({done: false});
 		},
-		nextOrder: function() {
+		nextOrder: function() {//下一个ORDER
 			if(!this.length) return 1;
 			return this.last().get('order') + 1;
 		},
-		comparator: 'order'
+		comparator: 'order'//以ORDER排序
 	});
 	
-	var Todos = new TodoList;
+	var Todos = new TodoList;//new一个任务列表
 	
 	//View
-	var TodoView = Backbone.View.extend({
+	var TodoView = Backbone.View.extend({//单个任务视图
 		tagName: "li",
-		template: _.template($('#item-template').html()),
-		events: {
+		template: _.template($('#item-template').html()),//单个任务列表模板
+		events: {//事件集
 			"click .toggle": "toggleDone",
 			"dblclick .view": "edit",
 			"click a.destroy": "clear",
 			"keypress .edit": "updateOnEnter",
 			"blur .edit": "close"
 		},
-		initialize: function() {
-			this.listenTo(this.model, "change", this.render);
+		initialize: function() {//初始化
+			console.log(this.model);
+			this.listenTo(this.model, "change", this.render);//他怎么知道this.model是谁的?因为line115
 			this.listenTo(this.model, "destroy", this.remove);
 		},
-		render: function() {
+		render: function() {//负责页面渲染
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('done', this.model.get('done'));
 			this.input = this.$(".edit");
 			return this;
 		},
-		toggleDone: function() {
+		toggleDone: function() {//更改任务的Done
 			this.model.toggle();
 		},
-		edit: function() {
+		edit: function() {//编辑任务
 			this.$el.addClass("editing");
 			this.input.focus();
 		},
-		close: function() {
+		close: function() {//关闭编辑
 			var value = this.input.val();
 			if(!value) {
 				this.clear();
@@ -67,10 +68,10 @@ $(function() {
 				this.$el.removeClass("editing");
 			}
 		},
-		updateOnEnter: function(e) {
+		updateOnEnter: function(e) {//确认编辑
 			if(e.keyCode == 13) this.close();
 		},
-		clear: function() {
+		clear: function() {//清除任务
 			this.model.destroy();	
 		}
 	});
@@ -83,7 +84,7 @@ $(function() {
 			"click #clear-completed": "clearCompleted",
 			"click #toggle-all": "toggleAllComplete"
 		},
-		initialize: function() {
+		initialize: function() {//初始化
 			this.input = this.$("#new-todo")	;
 			this.allCheckbox = this.$("#toggle-all")[0];
 			this.listenTo(Todos, "add", this.addOne);
@@ -95,7 +96,7 @@ $(function() {
 			
 			Todos.fetch();
 		},
-		render: function() {
+		render: function() {//渲染
 			var done = Todos.done().length;
 			var remaining = Todos.remaining().length;
 			
@@ -111,7 +112,7 @@ $(function() {
 			this.allCheckbox.checked = !remaining;
 		},
 		addOne: function(todo) {
-			var view = new TodoView({model: todo});
+			var view = new TodoView({model: todo});//这里将model传入啦
 			this.$("#todo-list").append(view.render().el);
 		},
 		addAll: function() {
